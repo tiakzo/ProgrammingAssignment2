debda3e7c1d206fbe20740c55fdef1ed149ce1d0
tiakzo <t_lezhava@yahoo.com>
2015-01-17 02:30:58 +0400

    this is already an editted code for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions serve to calculate the inverse of the matrix
+## and store them, so that if they are needed again, they can easily be
+## retrieved instead of recalculating it, which might take a while.
 
-## Write a short comment describing this function
+## Next is the function that caches the matrix. It has creates a list 
+## that containts function to get and set the value of the matrix and
+## get an set the value of the inverse of that matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(X = matrix()) {
+  
+      M=NULL
+      setmatrix<-function(y){
+            X<<-y
+            M<<-NULL
+      }
+      getmatrix<-function() X
+      setinverse<-function(solve) M<<-solve
+      getinverse<-function() M
+      
+      list(setmatrix=setmatrix, getmatrix=getmatrix,
+           setinverse=setinverse, getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## The following function first checks if the inverse of the
+## matrix is already in cache, and if so, the function gets
+## the inverse from there, if not, it calculates the inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(X, ...) {
+        
+      M<-X$getinverse()
+      if(!is.null(M)){
+            message("got from the cached data")
+            M
+            ##this is complete alternative of 'return(m)' if
+            ##the rest of the function is embedded in 'else'.
+      }
+      else{
+            data<-X$getmatrix()
+            M<-solve(data,...)
+            X$setinverse(M)
+            M
+      }
+      
 }
